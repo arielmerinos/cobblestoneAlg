a3166acef381b38005ab1e388211fe3b329ee42d
arielmerin <arielmerino@ciencias.unam.mx>
2022-03-09 13:56:00 -0600

    💄 Cleaning the color pallete
    Use a diferent color pallete

--- Color.py
+++ Color.py
@@ -5,14 +5,26 @@ class Color:
     def __init__(self):
         self.color_values = [
             "#F6E7D8",
-            '#F68989',
-            '#C65D7B',
-            '#874356',
+            '#7976e8',
+            '#6764c6',
+            '#8784ff',
+            '#5899fb',
             '#0E3EDA',
+            '#3a5aff',
+            '#0c36be',
+            '#0b2ea2',
+            '#09278b',
+            '#641f8b',
+            '#8e00ce',
+            '#8400bd',
+            '#c15e90',
+            '#b53086',
+            '#ff45bc',
+            '#e26aab',
             '#F473B9',
+            '#ffaad4',
             '#FFBDE6',
             '#FFDDEE',
-            '#E2DEA9',
         ]
 
     def colorear(self, identificador: int):
--- utils.py
+++ utils.py
@@ -5,5 +5,6 @@ def Log2(x):
     return (math.log10(x) /
             math.log10(2));
 
+
 def isPowerOfTwo(n):
     return (math.ceil(Log2(n)) == math.floor(Log2(n)));
